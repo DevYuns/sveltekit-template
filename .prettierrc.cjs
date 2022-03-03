30a913ed37798df9658124b5ad859aadf87e6bfc
DevYuns <ydhun93@gmail.com>
2022-03-03 12:23:58 +0900

    Setup prettier & eslint

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  tabWidth: 2,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  singleQuote: true,
+  jsxSingleQuote: false,
+  bracketSpacing: false,
+  svelteSortOrder: 'options-styles-scripts-markup',
+  svelteStrictMode: false,
+  svelteIndentScriptAndStyle: true,
+  svelteAllowShorthand: true,
+  svelteBracketNewLine: true,
+};
